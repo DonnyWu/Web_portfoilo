83002421431ab1bd55b587bdaca466ce53a5008a
DonnyWu <Donny.wu4@gmail.com>
2023-04-13 22:33:51 -0400

    Added text and profile picture to Home component

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,38 @@
 import React from 'react'
-import {HiArrowNarrowRight} from 'react-icons/fa'
+import {HiArrowNarrowRight} from 'react-icons/hi'
 
 const Home = () => {
   return (
-    <div className='w-full h-screen bg-[#0a192f]'> 
+    <div className='w-full h-screen bg-[#0a192f]'>
+
+      {/* <div className=" max-w-[800px] px-3 mx-auto flex flex-wrap justify-center blocl relative">
+        <div className="w-6/12 sm:w-4/12 px-4">
+          <img src="https://media.licdn.com/dms/image/C4E03AQFn7DuU9UN8yQ/profile-displayphoto-shrink_800_800/0/1602495882730?e=1686787200&v=beta&t=C_MODcN7kVnwrSAEWjmfK6XR7g89ZVlmmg36mRduZUE" alt="Profile Picture" className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+        </div>
+      </div> */}
+      
+      <div className='max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full'>
+
+        <div className=" max-w-[800px] px-3 mx-auto flex flex-wrap justify-start blocl relative">
+          <div className="w-6/12 sm:w-4/12 px-4">
+            <img src="https://media.licdn.com/dms/image/C4E03AQFn7DuU9UN8yQ/profile-displayphoto-shrink_800_800/0/1602495882730?e=1686787200&v=beta&t=C_MODcN7kVnwrSAEWjmfK6XR7g89ZVlmmg36mRduZUE" alt="Profile Picture" className="shadow rounded-full max-w-full h-auto align-middle border-none" />
+          </div>
+        </div>
+        
+        <p className='text-pink-600'>Hi, My name is</p>
+        <h1 className='text-4xl sm:text-7xl font-bold text-[#ccd6f6]'>Donnie Wu</h1>
+        <h2 className='text-4xl sm:text-7xl font-bold text-[#8892b0]'>I am a full stack developer</h2>
+        <p className='text-[#8892b0] py-4 max-w-[700px]'>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
+        <div>
+          <button className='text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600'>
+            view work
+            <span className='group-hover:rotate-90 duration-300'>
+              <HiArrowNarrowRight className='ml-3'  />
+            </span>
+          </button>
+        </div>
+      </div>
+
     </div>
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -31,7 +31,7 @@ const Navbar = () => {
         <li className='py-6 text-4xl'>Contact</li>
       </ul>
     
-      <div className='flex fixed flex-col top-[35%] left-0'>
+      {/* <div className='flex fixed flex-col top-[35%] left-0'>
     <ul>
       <li className='w-[160px] h-[100px]flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'> 
         <a className='flex justify-between items-center w-full text-gray-300' href="https://www.linkedin.com/in/donnie-wu/">
@@ -50,6 +50,46 @@ const Navbar = () => {
       </li>
     </ul>
 
+      </div> */}
+
+    
+
+
+      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+        <ul>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Linkedin <FaLinkedin size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Github <FaGithub size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Email <HiMailOpen size={30} />
+            </a>
+          </li>
+          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+            <a
+              className='flex justify-between items-center w-full text-gray-300'
+              href='/'
+            >
+              Resume <BsFillPersonLinesFill size={30} />
+            </a>
+          </li>
+        </ul>
       </div>
 
     </div>
