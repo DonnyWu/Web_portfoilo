4901a0451c70d2507f74fd3b250233cb52ff71ac
Don <don@dons-air.mynetworksettings.com>
2023-05-16 23:55:54 -0400

    Completed About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -12,6 +12,24 @@ const About = () => {
           </div>
           <div></div>
         </div>
+        <div className="max-w-[1000px] w-full sm:grid grid-cols-2 gap-8 px-4">
+          <div className="sm:text-right text-4xl font-bold">
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+            </p>
+          </div>
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
+              eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut
+              enim ad minim veniam, quis nostrud exercitation ullamco laboris
+              nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in
+              reprehenderit in voluptate velit esse cillum dolore eu fugiat
+              nulla pariatur. Excepteur sint occaecat cupidatat non proident,
+              sunt in culpa qui officia deserunt mollit anim id est laborum.
+            </p>
+          </div>
+        </div>
       </div>
     </div>
   );
