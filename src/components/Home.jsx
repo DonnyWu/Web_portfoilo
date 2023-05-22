d301cf1ff008061fff08319221f28e252a4541ed
Don <Donny.wu4@gmail.com>
2023-05-21 23:57:01 -0400

    Added react scroll

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 
 const Home = () => {
   return (
-    <div className="w-full h-screen bg-[#0a192f]">
+    <div name="home" className="w-full h-screen bg-[#0a192f]">
       {/* <div className=" max-w-[800px] px-3 mx-auto flex flex-wrap justify-center blocl relative">
         <div className="w-6/12 sm:w-4/12 px-4">
           <img src="https://media.licdn.com/dms/image/C4E03AQFn7DuU9UN8yQ/profile-displayphoto-shrink_800_800/0/1602495882730?e=1686787200&v=beta&t=C_MODcN7kVnwrSAEWjmfK6XR7g89ZVlmmg36mRduZUE" alt="Profile Picture" className="shadow rounded-full max-w-full h-auto align-middle border-none" />
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,36 +1,51 @@
-import React, {useState} from 'react';
-import {FaBars, FaTimes, FaGithub, FaLinkedin,FaLink} from 'react-icons/fa';
+import React, { useState } from "react";
+import { FaBars, FaTimes, FaGithub, FaLinkedin, FaLink } from "react-icons/fa";
 import { HiMailOpen } from "react-icons/hi";
-import {BsFillPersonLinesFill} from 'react-icons/bs';
+import { BsFillPersonLinesFill } from "react-icons/bs";
+import { Link } from "react-scroll";
 
 const Navbar = () => {
-  const [nav, setNav] = useState(false)
-  const handleClick = () => setNav(!nav)
+  const [nav, setNav] = useState(false);
+  const handleClick = () => setNav(!nav);
 
   return (
-    <div className='fixed w-full h-[80px] flex justify-end items-center px-4 bg-[#0a192f] text-gray-300'>
-
-      
-      <ul className='hidden md:flex'>
-        <li>Home</li>
-        <li>Resume</li>
-        <li>Contact</li>
+    <div className="fixed w-full h-[80px] flex justify-end items-center px-4 bg-[#0a192f] text-gray-300">
+      <ul className="hidden md:flex">
+        <li>
+          <Link to="home" smooth={true} duration={500}>
+            Home
+          </Link>
+        </li>
+        <li>
+          <Link to="skills" smooth={true} duration={500}>
+            Skills
+          </Link>
+        </li>
+        <li>
+          <Link to="contact" smooth={true} duration={500}>
+            Contact
+          </Link>
+        </li>
       </ul>
 
-
-      
-      <div onClick={handleClick} className='md:hidden z-10'>
+      <div onClick={handleClick} className="md:hidden z-10">
         {!nav ? <FaBars /> : <FaTimes />}
       </div>
 
       {/* mobile menu */}
 
-      <ul className={!nav ? 'hidden' : 'absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center'}>
-        <li className='py-6 text-4xl'>Home</li>
-        <li className='py-6 text-4xl'>Resume</li>
-        <li className='py-6 text-4xl'>Contact</li>
+      <ul
+        className={
+          !nav
+            ? "hidden"
+            : "absolute top-0 left-0 w-full h-screen bg-[#0a192f] flex flex-col justify-center items-center"
+        }
+      >
+        <li className="py-6 text-4xl">Home</li>
+        <li className="py-6 text-4xl">Resume</li>
+        <li className="py-6 text-4xl">Contact</li>
       </ul>
-    
+
       {/* <div className='flex fixed flex-col top-[35%] left-0'>
     <ul>
       <li className='w-[160px] h-[100px]flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-500'> 
@@ -52,48 +67,44 @@ const Navbar = () => {
 
       </div> */}
 
-    
-
-
-      <div className='hidden lg:flex fixed flex-col top-[35%] left-0'>
+      <div className="hidden lg:flex fixed flex-col top-[35%] left-0">
         <ul>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-blue-600">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Linkedin <FaLinkedin size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#333333]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Github <FaGithub size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#6fc2b0]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Email <HiMailOpen size={30} />
             </a>
           </li>
-          <li className='w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]'>
+          <li className="w-[160px] h-[60px] flex justify-between items-center ml-[-100px] hover:ml-[-10px] duration-300 bg-[#565f69]">
             <a
-              className='flex justify-between items-center w-full text-gray-300'
-              href='/'
+              className="flex justify-between items-center w-full text-gray-300"
+              href="/"
             >
               Resume <BsFillPersonLinesFill size={30} />
             </a>
           </li>
         </ul>
       </div>
-
     </div>
-  )
-}
+  );
+};
 
-export default Navbar
+export default Navbar;
