f8b83b7f32f837254da1ff0cbc7b84d09bdc4ed4
Don <Donny.wu4@gmail.com>
2023-05-29 22:16:29 -0400

    Added dynamic text

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,8 +1,22 @@
 import React from "react";
 import { View, Image, StyleSheet } from "react-native";
 import { HiArrowNarrowRight } from "react-icons/hi";
+import "../App.css";
 
 const styles = StyleSheet.create({
+  container: {
+    paddingTop: 50,
+    paddingBottom: 50,
+  },
+  stretch: {
+    width: 300,
+    height: 300,
+    borderRadius: 400 / 2,
+    resizeMode: "stretch",
+  },
+});
+
+const dynamic = StyleSheet.create({
   container: {
     paddingTop: 50,
     paddingBottom: 50,
@@ -18,13 +32,32 @@ const styles = StyleSheet.create({
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
-      {/* <div className=" max-w-[800px] px-3 mx-auto flex flex-wrap justify-center blocl relative">
-        <div className="w-6/12 sm:w-4/12 px-4">
-         < img src="https://media.licdn.com/dms/image/C4E03AQFn7DuU9UN8yQ/profile-displayphoto-shrink_800_800/0/1602495882730?e=1686787200&v=beta&t=C_MODcN7kVnwrSAEWjmfK6XR7g89ZVlmmg36mRduZUE" alt="Profile Picture" className="shadow rounded-full max-w-full h-auto align-middle border-none" />
-        </div>
-      </div> */}
-
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
+        <div className="Message">
+          <ul className="dynamic text-1xl">
+            <li>
+              <span>Hello World!</span>
+            </li>
+            <li>
+              <span>你好, 世界!</span>
+            </li>
+            <li>
+              <span>안녕하세요!</span>
+            </li>
+            <li>
+              <span>مرحبا بالعالم!</span>
+            </li>
+            <li>
+              <span>Hallo Welt!</span>
+            </li>
+            <li>
+              <span>안녕하세요!</span>
+            </li>
+            <li>
+              <span>Hej världen!</span>
+            </li>
+          </ul>
+        </div>
         <View style={styles.container}>
           <Image
             style={styles.stretch}
