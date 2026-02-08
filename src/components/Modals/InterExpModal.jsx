4f8452ff704f8dfdcb4ddc15325c23841088e55a
DonnyWu <donny.wu4@gmail.com>
2026-02-07 23:51:42 -0500

    Feature--Revised Modal Descriptions

--- src/components/Modals/InterExpModal.jsx
+++ src/components/Modals/InterExpModal.jsx
@@ -18,10 +18,14 @@ const InterModal = ({ open, onClose }) => {
           <h1 className='title'>Details</h1>
           {/* Content Details */}
           <div className='content contentFont'>
-            <p>{'\u2022'} Implemented InterPro Solutions’ flagship application EZMaxMobile based on client’s requirements using <br/>Java, Spring Boot, JSON, Angular, Struts, Typescript, and IBM Maximo custom Business Object APIs.</p>
+            {/* <p>{'\u2022'} Implemented InterPro Solutions’ flagship application EZMaxMobile based on client’s requirements using <br/>Java, Spring Boot, JSON, Angular, Struts, Typescript, and IBM Maximo custom Business Object APIs.</p>
             <p>{'\u2022'} Used React JS to customize the layout of InterPro Solutions’ EZMaxRequest</p>
             <p>{'\u2022'} Used developer environment to test the UI of EZMaxMobile, confirming it is crossed-referenced with IBM Maximo</p>
-            <p>{'\u2022'} Handle incoming tickets pertaining to client’s custom built EZMaxMobile with support team and team architect</p>
+            <p>{'\u2022'} Handle incoming tickets pertaining to client’s custom built EZMaxMobile with support team and team architect</p> */}
+            <p>{'\u2022'}Developed custom mobile solutions for InterPro’s flagship application, EZMaxMobile, integrating Java, Spring Boot, and Angular with IBM Maximo custom Business Object APIs</p>
+            <p>{'\u2022'}Refined the user interface for EZMaxRequest using React JS to deliver a custom build based on customer requirements</p>
+            <p>{'\u2022'}Executed UI/UX testing in a developer environment to ensure cross-referencing and data integrity with IBM Maximo backend systems</p>
+            <p>{'\u2022'}Collaborated with architects and support teams to resolve complex technical tickets, providing high-level troubleshooting for client-specific EZMaxMobile configurations</p>
           </div>
           <div className='btnContainer'>
             <button className='btnOutline' onClick={onClose}>
