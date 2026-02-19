95d4fbfaf321eaa9487da2b80ff401689ec2dd40
DonnyWu <donny.wu4@gmail.com>
2026-02-19 00:25:21 -0500

    Feature--Created a "Polaroid" Styled border around modal images. [AI Assisted]

--- src/components/Modals/AllscriptsExpModal.jsx
+++ src/components/Modals/AllscriptsExpModal.jsx
@@ -13,7 +13,10 @@ const AllscriptModal = ({ open, onClose }) => {
         className='modalContainer'
       >
         {/* Experince logo */}
-        <img className='companyImg' src={image} alt='/' />
+        <div className="polaroidWrap">
+          <img className='companyImg' src={image} alt='/' />
+          <div className="polaroidCaption">test</div>
+        </div>
         <div className='modalRight'>
           <h1 className='title'>Details</h1>
           {/* Content Details */}
