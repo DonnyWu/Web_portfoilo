c268a0dc737da001bb5c3b8b5ae2d352b17c2419
DonnyWu <donny.wu4@gmail.com>
2026-04-22 00:46:12 -0400

    fix(navbar): remove unused BsFillPersonLinesFill import

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,6 @@ import React, { useState } from "react";
 import { FaBars, FaTimes, FaGithub, FaLinkedin } from "react-icons/fa";
 import { GrNotes } from "react-icons/gr";
 import { HiMailOpen } from "react-icons/hi";
-import { BsFillPersonLinesFill } from "react-icons/bs";
 import { Link } from "react-scroll";
 import pdf from '../Documents/Donnies_Resume_May_2026.pdf';
 
